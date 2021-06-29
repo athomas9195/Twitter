47d4f8639101663dfc30ef4d1859e1cd134d241e
athomas9195 <anna.tina.thomas@gmail.com>
2021-06-28 18:42:07 -0700

    log out

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -7,7 +7,9 @@
 
 
 #import "TimelineViewController.h"
-#import "APIManager.h"  
+#import "APIManager.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
  
 @interface TimelineViewController ()
 
@@ -37,6 +39,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)didLogOut {
+    // TimelineViewController.m
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    
+    [[APIManager shared] logout];
+} 
+ 
 /*
 #pragma mark - Navigation
 

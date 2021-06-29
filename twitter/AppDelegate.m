01c7d295d832eae591a72200079f8e3051c45d4a
athomas9195 <anna.tina.thomas@gmail.com>
2021-06-28 18:37:15 -0700

    sign in

--- twitter/AppDelegate.m
+++ twitter/AppDelegate.m
@@ -11,10 +11,9 @@
 
 @interface AppDelegate ()
 
-@end
+@end 
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     

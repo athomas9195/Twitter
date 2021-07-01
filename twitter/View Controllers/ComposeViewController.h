eca414eb14ecaa4cd1d11d13aa9aadb251bd4e6d
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-01 11:42:07 -0700

    compose

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,18 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by Anna Thomas on 7/1/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,39 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Anna Thomas on 7/1/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+- (IBAction)closeBarButton:(id)sender {
+}
+
+- (IBAction)tweetBarButton:(id)sender {
+}
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
